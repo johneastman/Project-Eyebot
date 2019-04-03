b028822ddeec697694c7ba1c0e2427ff5421181f
John Eastman <jp.robotics@gmail.com>
2019-04-03 17:11:10 -0400

    code cleanup/comments

--- agent.py
+++ agent.py
@@ -1,12 +1,4 @@
-"""This is an artificial agent that plays "Fallout: New Vegas".
-
-NOTICE: This file has been modified from its original version. See below:
-coding: utf-8
-Object Detection Demo
-License: Apache License 2.0 (https://github.com/tensorflow/models/blob/master/LICENSE)
-source: https://github.com/tensorflow/models
-
-Copyright 2018 Caitlin Chapdelaine and John Eastman
+"""Copyright 2018 Caitlin Chapdelaine and John Eastman
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,6 +11,16 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+---
+
+This is the artificial agent that plays "Fallout: New Vegas".
+
+NOTICE: This file has been modified from its original version. See below:
+coding: utf-8
+Object Detection Demo
+License: Apache License 2.0 (https://github.com/tensorflow/models/blob/master/LICENSE)
+source: https://github.com/tensorflow/models
 """
 import numpy as np
 import os
@@ -31,6 +33,17 @@ from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
 import direct_keys  # direct_keys.py
+# import navigation_predictions  # navigation_predictions.py
+
+def predict(model, image):
+
+    width = 75
+    height = 133
+    
+    new_screen = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    new_screen = cv2.resize(new_screen, (width, height))
+    action = model.predict([new_screen.reshape(1, width, height)])
+    return action[0]
     
 # Load the navigation model
 
@@ -40,7 +53,7 @@ PATH_TO_LABELS = os.path.join(os.getcwd(), "fallout_inference_graph\\labelmap.pb
 # Path to frozen detection graph.
 PATH_TO_CKPT = os.path.join(os.getcwd(), "fallout_inference_graph\\frozen_inference_graph.pb")
 
-# Number of objects in the model
+# Number of objects in the object-detection model
 NUM_CLASSES = 5
 
 detection_graph = tf.Graph()
@@ -50,7 +63,7 @@ with detection_graph.as_default():
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
-
+        
 # Label maps map indices to category names, so that when our convolution network
 # predicts `5`, we know that this corresponds to `airplane`.  Here we use internal
 # utility functions, but anything that returns a dictionary mapping integers to
@@ -59,26 +72,34 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-enemy_attack_confidence = 0.94
-
 bounding_box = (8, 32, 808, 482)
 WIDTH =  bounding_box[2]
 HEIGHT = bounding_box[3]
 
 within_range = False  # Whether the agent is a certain distance from an enemy or not
-is_combat_mode = False
-nav_timer_reset = False
 
 back_key = direct_keys.S
 forward_key = direct_keys.W
 
+# Navigation agent data
 prev_key, prev_mouse = ("W", "none")
-model = tf.keras.models.load_model("navigation.model")
+actions = [("W", "right"),   ("W", "left"),   ("W", "none"),
+           ("A", "right"),   ("A", "left"),   ("A", "none"),
+           ("S", "right"),   ("S", "left"),   ("S", "none"),
+           ("D", "right"),   ("D", "left"),   ("D", "none"),
+           ("NIL", "right"), ("NIL", "left"), ("NIL", "none")]
 
-start_time = time.time()
+# How much to move the mouse for each direction
+to_move = {"right": -10,
+           "left":  10,
+           "none":  0}
 
 with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
+    
+        # Load navigation model
+        navigation_model = tf.keras.models.load_model("navigation_239.model")
+    
         while True:
 
             # Capture the screen
@@ -113,22 +134,21 @@ with detection_graph.as_default():
                 category_index,
                 use_normalized_coordinates=True,
                 line_thickness=8)
-
+                
             # Our code starts here
             enemy_info = []
             for i, b in enumerate(boxes[0]):
-                if scores[0][i] >= enemy_attack_confidence:
+                if scores[0][i] >= 0.90:
                     mid_x = ((boxes[0][i][1] + boxes[0][i][3]) / 2)
                     mid_y = ((boxes[0][i][0] + boxes[0][i][2]) / 2)
-
+                    
                     # Distance 
                     distance = round((1 - (boxes[0][i][3] - boxes[0][i][1])) ** 4, 3)
                     enemy_info.append((distance, mid_x, mid_y))
-
+                    
+                    # cv2.circle(image_np,(mid_x, mid_y), 3, (0,0,255), -1)
             if len(enemy_info) > 0:  # If an enemy has been detected
             
-                is_combat_mode = True
-                
                 direct_keys.release_key(forward_key)
                 
                 # Retrieve the coordinates of the closest enemy
@@ -162,26 +182,31 @@ with detection_graph.as_default():
                     within_range = True
                 else:
                     within_range = False
-                    
-            # Navigation Agent
+            
             else:
-                if not nav_timer_reset:
-                    navigation_start_time = time.time()
-                    nav_timer_reset = True
+                # Navigation Agent: 
+                # This agent is triggered when there are no enemies detected
+                # on the screen.
+                predicted_action = predict(navigation_model, image_np)
+                key, mouse = actions[np.argmax(predicted_action)]
                 
-                direct_keys.press_key(forward_key)
+                # Release the previously-pressed key
+                direct_keys.release_key(prev_key)
+                
+                direct_keys.press_key(key)
+                direct_keys.move_mouse_over_time(to_move[mouse])
+                
+                # Update the previous key and mouse presses
+                prev_key = key
+                prev_mouse = mouse
 
-            if is_combat_mode and time.time() - navigation_start_time >= 3:
-                direct_keys.key_sequence(f"~player.SetAngle X 0\n~".upper())
-                navigation_start_time = time.time()
-                is_combat_mode = False
-                nav_timer_reset = False
-                
             # Release the back key if the enemy is no longer within range
             if not within_range:
                 direct_keys.release_key(back_key)
 
+            '''
             cv2.imshow("window", image_np)
             if cv2.waitKey(25) & 0xFF == ord("q"):
                 cv2.destroyAllWindows()
                 break
+            '''
--- direct_keys.py
+++ direct_keys.py
@@ -1,6 +1,4 @@
-"""Utility for simulating keyboard and mouse input.
-
-Copyright 2018 Caitlin Chapdelaine and John Eastman
+"""Copyright 2018 Caitlin Chapdelaine and John Eastman
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,30 +11,33 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
 
-# Key Codes: http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+---
+
+This is a utility for simulating keyboard and mouse input.
+
+Useful information on how to use the keybd_event function:
+https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-keybd_event
+
+Usefun information on how to use the mouse_event function:
+https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-mouse_event
+
+Windows keyboard key codes:
+http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+"""
 import ctypes
 import time
 import random
 import win32gui
 import win32api
 
-# Keys
+# Commonly-used keys
 W = "W"
 A = "A"
 S = "S"
 D = "D"
 
-MOUSE_SLEEP = 0.001
-
-
-def get_mouse_pos():
-    _, _, pos = win32gui.GetCursorInfo()
-    return pos
-
-# Used for detecting whether the mouse has moved or not
-previous_position = get_mouse_pos()
+MOUSE_SLEEP = 0.005
 
 direct_keys = {"A": 0x1E,
                "B": 0x30,
@@ -79,17 +80,11 @@ direct_keys = {"A": 0x1E,
                "\n": 0x1C,
                "~": 0x29}
 
-# direct_keys  = {W: 0x11, A: 0x1E, S: 0x1F, D: 0x20}
-
 mouse_clicks = {"left_down": 0x2, "left_up": 0x4,
                 "right_down":0x8, "right_up": 0x10, "no_click": 0}
-                
-def is_key_pressed(key):
-    return win32api.GetAsyncKeyState(ord(key)) != 0
 
 def press_mouse(button="no_click", dx=0, dy=0):
     # mouse_event usage:
-    # https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-mouse_event
     if button not in mouse_clicks:
         raise KeyError(f"{button} is an invalid button")
     
@@ -99,25 +94,23 @@ def press_mouse(button="no_click", dx=0, dy=0):
     ctypes.windll.user32.mouse_event(mouse_button_hex_code, dx, dy, 0, 0)
 
 def press_key(key):
-    # Get the direct-key hex-code associated with 'key'.
-    # keybd_event usage:
-    # https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-keybd_event
+    """Press a keyboard key."""
     if key not in direct_keys:
         raise KeyError(f"{key} is an invalid key")
     key_hex_code = direct_keys[key]
     ctypes.windll.user32.keybd_event(0, key_hex_code, 0, 0)
 
 def release_key(key):
-    # Get the direct-key hex-code associated with 'key'.
-    # keybd_event usage:
-    # https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-keybd_event
-    # 0x0002 flag indicates that key is being released.
+    """Release a pressed keyboard key."""
     if key not in direct_keys:
         raise KeyError(f"{key} is an invalid key")
     
+    # Get the direct-key hex-code associated with 'key'.
     key_hex_code = direct_keys[key]
-    ctypes.windll.user32.keybd_event(0, key_hex_code, 0x0002, 0)
     
+    # 0x0002 flag indicates that key is being released.
+    ctypes.windll.user32.keybd_event(0, key_hex_code, 0x0002, 0)
+'''
 def key_sequence(string):
     """Press all the keyboard keys in a string of keys.
     
@@ -128,44 +121,13 @@ def key_sequence(string):
         press_key(key)
         time.sleep(0.1)
         release_key(key)
-   
-def move_mouse_over_time(dx):  
+'''  
+def move_mouse_over_time(dx):
+    """Move the mouse over a period of time.
+    
+    This allows for smooth mouse movements that would look similar to a human
+    moving the mouse.
+    """
     for i in range(50):
         press_mouse(dx=dx)
         time.sleep(MOUSE_SLEEP)
-
-
-def get_mouse_direction():
-    """Return the direction the mouse is moving.
-    
-    Note: Only detects left, right, or no movement
-    
-    "right" -> Moving right
-    "left"  -> Moving left
-    "none"  -> Mouse is not moving
-    """
-    global previous_position
-    
-    prev_x, prev_y = previous_position
-    curr_x, curr_y = get_mouse_pos()
-    
-    # Update the previous position of the mouse
-    previous_position = (curr_x, curr_y)
-    
-    # Width of screen. Minus 1 because resolution goes from 0 to (width - 1)
-    # e.g., if width is 1920, mouse goes from 0 to 1919
-    if (curr_x > prev_x or curr_x == win32api.GetSystemMetrics(0) - 1) and curr_y != prev_y:
-        return "right"
-    elif (curr_x < prev_x or curr_x == 0) and curr_y != prev_y:
-        return "left"
-    
-    return "none"
-    
-    # return not (prev_x == curr_x and prev_y == curr_y)
-
-if __name__ == "__main__":
-    
-    while True:
-        direction = get_mouse_direction()
-        print(direction)
-        time.sleep(0.1)
